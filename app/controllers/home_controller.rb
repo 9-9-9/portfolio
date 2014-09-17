49995207281033ea4a02959b20cbbfb8263fdeee
Matthew Bilbow <mtpbzz@Matthews-MacBook-Air.local>
2014-09-17 22:25:40 +0100

    Added shot limit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @player = Dribbble::Player.find('mattbilbow')
-    @shots = @player.shots
+    @shots = @player.shots(:per_page => 30)
   end
 
   def about
